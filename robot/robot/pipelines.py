3f49d8dd8f16105116b2e62f80330a3df8d052fa
changfengwangluo <3094219@qq.com>
2018-05-18 22:31:44 +0800

    优化了储存

--- robot/robot/pipelines.py
+++ robot/robot/pipelines.py
@@ -10,10 +10,8 @@ import re
 
 class RobotPipeline(object):
     def process_item(self, item, spider):
-        domain_name = item['domain_name']
-        result = WebInfo.objects.filter(domain_name=domain_name).first()
-        if result is None:
-            WebInfo.objects.create(domain_name=item['domain_name'], title=item['title'], keywords=item['keywords'],
+
+        WebInfo.objects.create(domain_name=item['domain_name'], title=item['title'], keywords=item['keywords'],
                                    desc=item['desc'])
         return item
 
